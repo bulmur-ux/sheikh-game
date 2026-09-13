d26f447e2bef04f2b0da4f1c16ff7b513183daa1
bulmur-ux <bulmur@hotmail.com>
2026-09-13 23:43:21 +0400

    Add sound effects for various actions

---
+++ sounds.js
@@ -0,0 +1,83 @@
+// شيك شيك - أصوات المجلس
+window.SHEIKH_FUN_SOUNDS = {
+  laugh: {
+    label: "😂 ضحك",
+    url: "https://www.myinstants.com/media/sounds/baby-laughing-meme.mp3"
+  },
+  wildlaugh: {
+    label: "🤣 ضحك قوي",
+    url: "https://www.myinstants.com/media/sounds/silly-screaming-laughter.mp3"
+  },
+  haha: {
+    label: "😆 ههههه",
+    url: "https://www.myinstants.com/media/sounds/ny-video-online-audio-converter.mp3"
+  },
+  bruh: {
+    label: "😑 براه",
+    url: "https://www.myinstants.com/media/sounds/bruh-meme-sound-effect.mp3"
+  },
+  boom: {
+    label: "💥 بوم",
+    url: "https://www.myinstants.com/media/sounds/vine-boom-meme_cCCpxZG.mp3"
+  },
+  damage: {
+    label: "💔 Emotional Damage",
+    url: "https://www.myinstants.com/media/sounds/emotional-damage-meme.mp3"
+  },
+  sad: {
+    label: "🎻 حزين",
+    url: "https://www.myinstants.com/media/sounds/tf_nemesis.mp3"
+  },
+  fart: {
+    label: "💨 فارت",
+    url: "https://www.myinstants.com/media/sounds/fart-meme-sound.mp3"
+  },
+  run: {
+    label: "🏃 اهرب",
+    url: "https://www.myinstants.com/media/sounds/run-vine-sound-effect.mp3"
+  },
+  snore: {
+    label: "😴 نام",
+    url: "https://www.myinstants.com/media/sounds/snore-mimimimimimi.mp3"
+  },
+  omg: {
+    label: "😱 Oh My God",
+    url: "https://www.myinstants.com/media/sounds/oh-my-god-meme.mp3"
+  },
+  wow: {
+    label: "🤩 واو",
+    url: "https://www.myinstants.com/media/sounds/anime-wow-sound-effect-mp3cut.mp3"
+  },
+  arab: {
+    label: "🪘 عربي مضحك",
+    url: "https://www.myinstants.com/media/sounds/arab-funny.mp3"
+  },
+  wth: {
+    label: "😳 وش ذا؟",
+    url: "https://www.myinstants.com/media/sounds/what-the-hell-meme-sound-effect.mp3"
+  },
+  ehh: {
+    label: "🤨 إيه بروذر",
+    url: "https://www.myinstants.com/media/sounds/ehh-brother-ehh.mp3"
+  },
+  tuco: {
+    label: "🚪 اطلع برا",
+    url: "https://www.myinstants.com/media/sounds/tuco-get-out.mp3"
+  },
+  pipe: {
+    label: "🔩 طاح الحديد",
+    url: "https://www.myinstants.com/media/sounds/metal-pipe-falling-sound-effect.mp3"
+  },
+  shutdown: {
+    label: "💻 خلاص سكّر",
+    url: "https://www.myinstants.com/media/sounds/windows-xp-shutdown.mp3"
+  },
+  connect: {
+    label: "🔌 شبك",
+    url: "https://www.myinstants.com/media/sounds/windows-10-usb-connect-38512.mp3"
+  },
+  achievement: {
+    label: "🏆 إنجاز",
+    url: "https://www.myinstants.com/media/sounds/xbox-one-rare-achievement-45050.mp3"
+  }
+};
