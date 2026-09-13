bade0628c3d717b8ed00560e5e0312673671c455
bulmur-ux <bulmur@hotmail.com>
2026-09-14 02:42:08 +0400

    Refactor sound categories and add new sounds
    Updated sound categories and labels in sounds.js to reflect Arabic translations and added new sounds.

--- sounds.js
+++ sounds.js
@@ -1,128 +1,129 @@
 // شيك شيك - أصوات المجلس
+// ملف الأصوات المستقل
 window.SHEIKH_FUN_SOUNDS = {
   laugh: {
     label: "😂 ضحك",
-    category: "laugh",
+    category: "ضحك وردود",
     url: "https://www.myinstants.com/media/sounds/baby-laughing-meme.mp3"
   },
 
   wildlaugh: {
     label: "🤣 ضحك قوي",
-    category: "laugh",
+    category: "ضحك وردود",
     url: "https://www.myinstants.com/media/sounds/silly-screaming-laughter.mp3"
   },
 
   haha: {
     label: "😆 ههههه",
-    category: "laugh",
-    url: "https://www.myinstants.com/media/sounds/ny-video-online-audio-converter.mp3"
+    category: "ضحك وردود",
+    url: "https://www.myinstants.com/media/sounds/funny-laughing-meme.mp3"
   },
 
   ohno: {
-    label: "😂 Oh No No No",
-    category: "laugh",
-    url: "https://www.myinstants.com/media/sounds/oh-no-no-no-laugh.mp3"
+    label: "😈 Oh No No No",
+    category: "ضحك وردود",
+    url: "https://www.myinstants.com/media/sounds/oh-no-no-no-no-laugh.mp3"
   },
 
   huh: {
     label: "🤨 HUH?",
-    category: "reaction",
+    category: "ضحك وردود",
     url: "https://www.myinstants.com/media/sounds/huh_37bAoRo.mp3"
   },
 
   bruh: {
-    label: "😑 Bruh",
-    category: "reaction",
+    label: "😑 براه",
+    category: "ضحك وردود",
     url: "https://www.myinstants.com/media/sounds/bruh-meme-sound-effect.mp3"
   },
 
-  ewbrother: {
-    label: "🤢 Eww Brother",
-    category: "reaction",
-    url: "https://www.myinstants.com/media/sounds/eww-brother-eww.mp3"
+  ehh: {
+    label: "🤨 إيه بروذر",
+    category: "ضحك وردود",
+    url: "https://www.myinstants.com/media/sounds/ehh-brother-ehh.mp3"
   },
 
   scream: {
-    label: "😱 AAAAH",
-    category: "shock",
+    label: "😱 صرخة",
+    category: "صرخات ومفاجآت",
     url: "https://www.myinstants.com/media/sounds/man-screaming-aaaah.mp3"
   },
 
-  omg: {
-    label: "😱 Oh My God",
-    category: "shock",
-    url: "https://www.myinstants.com/media/sounds/oh-my-god-meme.mp3"
-  },
-
   boom: {
-    label: "💥 Vine Boom",
-    category: "shock",
+    label: "💥 بوم",
+    category: "صرخات ومفاجآت",
     url: "https://www.myinstants.com/media/sounds/vine-boom-meme_cCCpxZG.mp3"
   },
 
   pipe: {
     label: "🔩 طاح الحديد",
-    category: "shock",
+    category: "صرخات ومفاجآت",
     url: "https://www.myinstants.com/media/sounds/metal-pipe-falling-sound-effect.mp3"
+  },
+
+  omg: {
+    label: "😱 Oh My God",
+    category: "صرخات ومفاجآت",
+    url: "https://www.myinstants.com/media/sounds/oh-my-god-meme.mp3"
   },
 
   yabn: {
     label: "😂 يا ابن اللعيبه",
-    category: "challenge",
+    category: "تحدي ومزح",
     url: "https://www.myinstants.com/media/sounds/y-bn-ll-ybh.mp3"
   },
 
   damage: {
     label: "💔 Emotional Damage",
-    category: "challenge",
-    url: "https://www.myinstants.com/media/sounds/emotional-damage-meme.mp3"
+    category: "تحدي ومزح",
+    url: "https://www.myinstants.com/media/sounds/emotional-damage_svaNMfN.mp3"
   },
 
-  getout: {
+  tuco: {
     label: "🚪 اطلع برا",
-    category: "challenge",
+    category: "تحدي ومزح",
     url: "https://www.myinstants.com/media/sounds/tuco-get-out.mp3"
+  },
+
+  wth: {
+    label: "😳 وش ذا؟",
+    category: "تحدي ومزح",
+    url: "https://www.myinstants.com/media/sounds/what-the-hell-meme-sound-effect.mp3"
   },
 
   run: {
     label: "🏃 اهرب",
-    category: "challenge",
+    category: "تحدي ومزح",
     url: "https://www.myinstants.com/media/sounds/run-vine-sound-effect.mp3"
-  },
-
-  wow: {
-    label: "🤩 واو",
-    category: "win",
-    url: "https://www.myinstants.com/media/sounds/anime-wow-sound-effect-mp3cut.mp3"
   },
 
   achievement: {
     label: "🏆 إنجاز",
-    category: "win",
-    url: "https://www.myinstants.com/media/sounds/xbox-one-rare-achievement-45050.mp3"
+    category: "فوز وخسارة",
+    url: "https://www.myinstants.com/media/sounds/xbox-one-rare-achievement-sound-popup-notification.mp3"
+  },
+
+  wow: {
+    label: "🤩 واو",
+    category: "فوز وخسارة",
+    url: "https://www.myinstants.com/media/sounds/anime-wow-sound-effect.mp3"
   },
 
   sad: {
     label: "🎻 حزين",
-    category: "lose",
-    url: "https://www.myinstants.com/media/sounds/sad-violin.mp3"
+    category: "فوز وخسارة",
+    url: "https://www.myinstants.com/media/sounds/tf_nemesis.mp3"
   },
 
   snore: {
     label: "😴 نام",
-    category: "lose",
+    category: "فوز وخسارة",
     url: "https://www.myinstants.com/media/sounds/snore-mimimimimimi.mp3"
   },
 
   shutdown: {
     label: "💻 خلاص سكّر",
-    category: "lose",
-    url: "https://www.myinstants.com/media/sounds/windows-xp-shutdown.mp3"
-  },
-
-  fart: {
-    label: "💨 فارت",
-    category: "funny",
-    url: "https://www.myinstants.com/media/sounds/fart-meme-sound.mp3"
+    category: "فوز وخسارة",
+    url: "https://www.myinstants.com/media/sounds/preview_4.mp3"
   }
 };
